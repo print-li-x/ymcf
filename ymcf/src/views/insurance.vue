9286ea79783c7061c03bd7a3152339e94dc0edd8
zjunbycy <164368257+zjunbycy@users.noreply.github.com>
2025-07-25 20:10:56 +0800

    初步构建内容

--- ymcf/src/views/insurance.vue
+++ ymcf/src/views/insurance.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        123
+        <insurance />
     </div>
 </template>
+
+<script setup>
+import insurance from '@/components/insuranceco.vue'
+</script>
+
+<style scoped>
+.page, .page :deep(#cesiumContainer){
+  width:100%;
+  height:calc(100vh - 56px); 
+}
+</style>
